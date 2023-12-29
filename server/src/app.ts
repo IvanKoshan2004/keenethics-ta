0a781daa36de9ebfec1faae6e6c1856e36b616dd
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-12-29 20:31:07 +0200

    added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 import bicycleRouter from "./routers/bicycle/bicycle.router";
 import validationFilter from "./lib/validation-error.filter";
-import mongoose from "mongoose";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ async function main() {
     const app = express();
     await mongoose.connect(MONGODB_URI);
     app.use(bodyParser.json());
+    app.use(cors());
     app.use("/bicycle", bicycleRouter);
     app.use(validationFilter);
     app.listen(PORT, () => {
