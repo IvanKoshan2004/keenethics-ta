8ad469a4b5d0b484e911f358dda8a990cf4256c0
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-12-31 18:45:45 +0200

    setting up a docker-compose for running the stack on docker

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,13 +6,13 @@ import mongoose from "mongoose";
 import bicycleRouter from "./routers/bicycle/bicycle.router";
 import validationFilter from "./lib/validation-error.filter";
 
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || "";
+dotenv.config({ path: process.env.ENV_FILE_PATH || ".env" });
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 async function main() {
     const app = express();
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI!);
     app.use(bodyParser.json());
     app.use(cors());
     app.use("/bicycle", bicycleRouter);
